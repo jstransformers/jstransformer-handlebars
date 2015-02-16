a75fb1dc4b003bfc0df0f1aab7dc38514503c076
hemanth <hemanth.hm@gmail.com>
2015-02-16 09:14:18 +0000

    Refactor wrt #1

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var Handlebars = require('handlebars');
 
 exports.name = 'handlebars';
 exports.outputFormat = 'html';
-exports.render = function (source, data) {
-  return Handlebars.compile(source)(data);
-};
+
+exports.compile = function (str, options) {
+  options = options || {};
+  for (var partial in options.partials) {
+    Handlebars.registerPartial(partial, options.partials[partial]);
+  }
+  for (var helper in options.helpers) {
+    Handlebars.registerHelper(helper, options.helpers[helper]);
+  }
+  return Handlebars.compile(str, options);
+}
--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ var transform = require('../');
 var input = fs.readFileSync(__dirname + '/input.hbs', 'utf8');
 var expected = fs.readFileSync(__dirname + '/expected.html', 'utf8');
 
-var output = transform.render(input,{name: 'jstransformer-handlebars'});
+var output = transform.compile(input,{})({name: 'jstransformer-handlebars'});
 fs.writeFileSync(__dirname + '/output.html', output);
 assert(output === expected, 'output.html should equal expected.html');
 
