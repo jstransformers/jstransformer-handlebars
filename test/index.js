a3ac5dec152022ffa0f955de966ad69a5768b097
tunnckoCore <mameto_100@mail.bg>
2015-04-05 18:43:36 +0300

    update tests

--- test/index.js
+++ test/index.js
@@ -1,15 +1,33 @@
 'use strict';
 
-var assert = require('assert');
-var fs = require('fs');
+var test = require('assertit');
+var transform = require('../index');
 
-var transform = require('../');
+test('should compile hbs file from given string (.compile)', function(done) {
+  var compiled = transform.compile('<p>Hello, my name is {{name}}.</p>');
+  var actual = compiled({name: 'jstransformer-handlebars'});
+  var expected = '<p>Hello, my name is jstransformer-handlebars.</p>';
 
-var input = fs.readFileSync(__dirname + '/input.hbs', 'utf8');
-var expected = fs.readFileSync(__dirname + '/expected.html', 'utf8');
+  test.equal(actual, expected);
+  done();
+});
 
-var output = transform.compile(input,{})({name: 'jstransformer-handlebars'});
-fs.writeFileSync(__dirname + '/output.html', output);
-assert(output === expected, 'output.html should equal expected.html');
+test('should compile from a given filepath, synchronously', function(done) {
+  var compiled = transform.compileFile('./test/fixture.hbs');
+  var actual = compiled({name: 'jstransformer-handlebars'});
+  var expected = '<p>Hello, my name is jstransformer-handlebars.</p>';
 
-console.log('test passed');
+  test.equal(actual, expected);
+  done();
+});
+
+test('should compile hbs file asynchronously (promise)', function(done) {
+  var promise = transform.compileFileAsync('./test/fixture.hbs');
+  var expected = '<p>Hello, my name is jstransformer-handlebars.</p>';
+
+  promise.then(function(compiled) {
+    var actual = compiled({name: 'jstransformer-handlebars'});
+    test.equal(actual, expected);
+    done();
+  });
+});
