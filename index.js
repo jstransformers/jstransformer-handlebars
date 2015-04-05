580903028d2777588fbc35106fef2345863a59d9
tunnckoCore <mameto_100@mail.bg>
2015-04-05 18:37:33 +0300

    add more compile methods

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
 var Handlebars = require('handlebars');
+var bluebird = require('bluebird');
+var readFile = bluebird.promisify(fs.readFile);
+var readFileSync = fs.readFileSync;
 
 exports.name = 'handlebars';
 exports.outputFormat = 'html';
 
-exports.compile = function (str, options) {
+exports.compile = function _compile(str, options) {
   options = options || {};
   for (var partial in options.partials) {
     Handlebars.registerPartial(partial, options.partials[partial]);
@@ -14,4 +19,13 @@ exports.compile = function (str, options) {
     Handlebars.registerHelper(helper, options.helpers[helper]);
   }
   return Handlebars.compile(str, options);
-}
+};
+exports.compileFile = function _compileFile(filepath, options) {
+  var str = readFileSync(filepath, 'utf8');
+  return exports.compile(str, options);
+};
+exports.compileFileAsync = function _compileFileAsync(filepath, options) {
+  return readFile(filepath, 'utf8').then(function(data) {
+    return Handlebars.compile(data, options);
+  });
+};
