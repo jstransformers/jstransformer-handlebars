e00efbed02f81865b63672c065536edd6d42c10d
tunnckoCore <mameto_100@mail.bg>
2015-04-05 18:51:56 +0300

    add tests for render methods

--- test/index.js
+++ test/index.js
@@ -31,3 +31,29 @@ test('should compile hbs file asynchronously (promise)', function(done) {
     done();
   });
 });
+
+test('should render hbs file from given string (.render)', function(done) {
+  var actual = transform.render('<p>Hello, my name is {{name}}.</p>', {name: 'jstransformer-handlebars'});
+  var expected = '<p>Hello, my name is jstransformer-handlebars.</p>';
+
+  test.equal(actual, expected);
+  done();
+});
+
+test('should render from a given filepath, synchronously', function(done) {
+  var actual = transform.renderFile('./test/fixture.hbs', {name: 'jstransformer-handlebars'});
+  var expected = '<p>Hello, my name is jstransformer-handlebars.</p>';
+
+  test.equal(actual, expected);
+  done();
+});
+
+test('should render hbs file asynchronously (promise)', function(done) {
+  var promise = transform.renderFileAsync('./test/fixture.hbs', {name: 'jstransformer-handlebars'});
+  var expected = '<p>Hello, my name is jstransformer-handlebars.</p>';
+
+  promise.then(function(actual) {
+    test.equal(actual, expected);
+    done();
+  });
+});
