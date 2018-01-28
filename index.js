84ba7b398e20bb973f807609549ae1fd25aa625f
Rob Loach <robloach@gmail.com>
2018-01-28 15:51:18 -0500

    1.1.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var Handlebars = require('handlebars')
+const Handlebars = require('handlebars')
 
 exports.name = 'handlebars'
 exports.inputFormats = ['hbs', 'handlebars']
@@ -9,17 +9,17 @@ exports.handlebars = Handlebars
 
 exports.compile = function (str, options) {
   options = options || {}
-  for (var partial in options.partials || {}) {
+  for (const partial in options.partials || {}) {
     if ({}.hasOwnProperty.call(options.partials, partial)) {
       Handlebars.registerPartial(partial, options.partials[partial])
     }
   }
-  for (var helper in options.helpers || {}) {
+  for (const helper in options.helpers || {}) {
     if ({}.hasOwnProperty.call(options.helpers, helper)) {
       Handlebars.registerHelper(helper, options.helpers[helper])
     }
   }
-  for (var decorator in options.decorators || {}) {
+  for (const decorator in options.decorators || {}) {
     if ({}.hasOwnProperty.call(options.decorators, decorator)) {
       Handlebars.registerDecorator(decorator, options.decorator[decorator])
     }
