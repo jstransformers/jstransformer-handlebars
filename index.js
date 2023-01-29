494c38dec4dc4aa29d329bf61a2833444a63ef95
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2023-01-29 01:24:10 +0100

    Updates xo and uses Object.prototype.hasOwnProperty instead of {}.hasOwnProperty

--- index.js
+++ index.js
@@ -2,30 +2,34 @@
 
 const Handlebars = require('handlebars')
 
-exports.name = 'handlebars'
-exports.inputFormats = ['hbs', 'handlebars']
-exports.outputFormat = 'html'
-exports.handlebars = Handlebars
+const transformer = {
+  name: 'handlebars',
+  inputFormats: ['hbs', 'handlebars'],
+  outputFormat: 'html',
+  handlebars: Handlebars,
+}
 
-exports.compile = function (str, options) {
+transformer.compile = function (source, options) {
   options = options || {}
   for (const partial in options.partials || {}) {
-    if ({}.hasOwnProperty.call(options.partials, partial)) {
+    if (Object.prototype.hasOwnProperty.call(options.partials, partial)) {
       Handlebars.registerPartial(partial, options.partials[partial])
     }
   }
 
   for (const helper in options.helpers || {}) {
-    if ({}.hasOwnProperty.call(options.helpers, helper)) {
+    if (Object.prototype.hasOwnProperty.call(options.helpers, helper)) {
       Handlebars.registerHelper(helper, options.helpers[helper])
     }
   }
 
   for (const decorator in options.decorators || {}) {
-    if ({}.hasOwnProperty.call(options.decorators, decorator)) {
+    if (Object.prototype.hasOwnProperty.call(options.decorators, decorator)) {
       Handlebars.registerDecorator(decorator, options.decorator[decorator])
     }
   }
 
-  return Handlebars.compile(str, options)
+  return Handlebars.compile(source, options)
 }
+
+module.exports = transformer
